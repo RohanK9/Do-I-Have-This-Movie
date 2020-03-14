4efbaf6bbc7c024e82b1e51841e426f1f7901f84
rohankharwadkar@ufl.edu <rohankharwadkar@ufl.edu>
2020-03-13 21:02:31 -0400

    First commit

---
+++ search.py
@@ -0,0 +1,22 @@
+import sqlite3
+# import populate_database
+
+connection = sqlite3.connect('filenames.db')
+cursor = connection.cursor()
+
+def searchTable(fileToSearchFor):
+
+   movieFound = False
+
+   # fileToSearchFor = input("Please enter the name of the movie you are looking for: ")
+   
+   searchResults = cursor.execute("SELECT * FROM files WHERE fileName LIKE ?", ('%'+fileToSearchFor+'%',))
+
+   for name in searchResults:
+      print(name[0])
+      movieFound = True
+
+   if not movieFound:
+      print("File does not exist")
+
+   return "Hello world"
